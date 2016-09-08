0cc786748f4af9adf17507b70b437717f055264a
Joaquin <joako.n96@gmail.com>
2016-09-08 19:15:58 -0300

    Ultimo

--- class1/example/public/app.js
+++ class1/example/public/app.js
@@ -1,8 +1,26 @@
 console.log("welcome to fligs search");
 
-function search(){
-	console.log("Buscando...");
-	
+
+
+
+function Header() {
+	return React.createElement('h1',null,
+		'Buscando vuelos...'
+	);
+}
+
+input = props =>
+	React.createElement('input',{type:props.type,placeholder:props.placeholder,id:props.id},null)
+
+
+button = props =>
+	React.createElement('button',{onClick:props.onclick},props.text)
+
+
+
+
+click = () => {
+	alert("hola mundo")
 	$.ajax({
 		method:"POST",
 		url:"/vuelos",
@@ -14,5 +32,19 @@ function search(){
 		success: (status, data)=>{
 			console.log(status, data);
 		}
-	});	
+	});
 }
+var inputs = [{type:'text',placeholder:'Desde...',id:'from'},
+	{type:'text',placeholder:'Hasta...',id:'to'},
+	{type:'number',placeholder:'1000',id:'price'}]
+
+
+var app = React.createElement('div',null,
+		React.createElement(Header),
+	React.createElement(button,{onclick:click,text:'Buscar'}),
+		inputs.map( element =>{
+			return React.createElement(input,element,null)
+		})
+);
+
+ReactDOM.render(app,document.getElementById('content'))
